f56f79a9116f4ec9169c1a9ac25c50747f7117a8
Rick Spencer <richard.linger.spencer.3@gmail.com>
2020-02-16 09:17:01 -0500

    added save function, and tested in load function

--- lib/src/persistence.dart
+++ lib/src/persistence.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 abstract class PersistenceProvider {
   saveDocument(Document doc);
@@ -16,7 +17,7 @@ class LocalFilePersistence implements PersistenceProvider {
   saveDocument(Document doc) async {
     final file = await _localFile(doc["_id"]);
     String mapString = json.encode(doc);
-    file.writeAsStringSync('$mapString');
+    file.writeAsStringSync(mapString);
   }
 
   Future loadDocuments(DocumentList documentList,
@@ -80,22 +81,41 @@ class LocalFilePersistence implements PersistenceProvider {
 }
 
 class SecretsPercistence implements PersistenceProvider {
+  final storage = new FlutterSecureStorage();
   @override
-  Future deleteDocument(Document doc) {
-    // TODO: implement deleteDocument
+  Future deleteDocument(Document doc) async {
     throw UnimplementedError();
   }
 
   @override
-  Future loadDocuments(DocumentList documentList, {Function onChangedListener}) {
-    // TODO: implement loadDocuments
-    throw UnimplementedError();
+  Future loadDocuments(DocumentList documentList,
+      {Function onChangedListener}) async {
+    Map<String, String> docMaps = await storage.readAll();
+    print("**** $docMaps");
+    // I'm following the rule of 2 here, duplicating this code
+    // It does seem like handling turning strings of json into
+    // documents should be done in one place
+    docMaps.forEach((String key, String value) {
+      if (key == documentList.documentType) {
+        if (value.length != 0) {
+          Map newData = json.decode(value);
+          newData.keys.forEach((dynamic key) {
+            if (key == "latlong" && newData[key] != null) {
+              // convert latlongs to the correct type
+              newData[key] = Map<String, double>.from(newData[key]);
+            }
+          });
+          Document loadedDoc = Document.fromMap(newData);
+          loadedDoc.addListener(documentList.notifyListeners);
+          documentList.add(loadedDoc, saveOnAdd: false);
+        }
+      }
+    });
   }
 
   @override
   saveDocument(Document doc) {
-    // TODO: implement saveDocument
-    throw UnimplementedError();
+    String mapString = json.encode(doc);
+    storage.write(key: doc.id, value: mapString);
   }
-
 }
